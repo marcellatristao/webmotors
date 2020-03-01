1bd01117be0ad135a0d122d8ce020f2c203a2d1d
marcellatristao <marcellacanedo@hotmail.com>
2020-03-01 10:48:49 -0300

    Implementação rect-redux

--- webmotors-frontend/src/App.js
+++ webmotors-frontend/src/App.js
@@ -1,24 +1,23 @@
 import React from 'react';
-import './App.css';
+import {connect} from 'react-redux';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+const Page = (props) =>{
+  return(
+    <div> Funcionando...</div>
+
   );
 }
+const mapStateToProps = (state) =>{
+  return{
+    user:state.user
+  };
+}
 
-export default App;
+const mapDispatchToProps = (dispatch) => {
+  return{
+  
+  };
+}
+
+
+export default connect (mapStateToProps, mapDispatchToProps)(Page);
