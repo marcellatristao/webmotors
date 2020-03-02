c4912c8734fc90a1df6b7be243deab16820187dc
marcellatristao <marcellacanedo@hotmail.com>
2020-03-02 00:10:12 -0300

    Início -> header

--- webmotors-frontend/src/components/partials/Header/index.js
+++ webmotors-frontend/src/components/partials/Header/index.js
@@ -1,10 +1,19 @@
 import React from 'react';
 import { HeaderArea } from './styled';
+import { Link } from 'react-router-dom';
+
+import webmotorslogo from '../Header/webmotorslogo.png';
 
 const Header = () => {
     return (
         <HeaderArea>
-            ...
+            <div className="container">
+                <div className="logo">
+                    <Link to="/">
+                        <img src={webmotorslogo} alt="Aqui jaz a logo do webmotors"/>
+                    </Link>
+                </div>
+            </div>
         </HeaderArea>
     );
 }
--- webmotors-frontend/src/components/partials/Header/styled.js
+++ webmotors-frontend/src/components/partials/Header/styled.js
@@ -1,6 +1,20 @@
 import styled from 'styled-components';
 
 export const HeaderArea = styled.div`
-    height: 100px;
-    background-color:#CCC;
+background-color: #FFFFFF;
+height: 60px;
+border-bottom: 1px solid #CCC;
+
+.container{
+    max-width: 1300px;
+    margin: auto;
+    display: flex;
+}
+
+.logo{
+    flex: 1;
+    display: flex;
+    align-itens: center;
+    margin-top: 15px;
+}
 `;
